a51f55959d50cacda30bdeaee9e12cea27d15047
BambiMC <fabians.viper@arcor.de>
2025-08-08 12:14:09 +0200

    fix setting token length based on model

--- JailbreakScan.py
+++ JailbreakScan.py
@@ -101,8 +101,9 @@ def load_model(model_name: str, load_in_4bit: bool = True, multi_gpu: bool = Fal
 def generate_batch_responses(prompts, tokenizer, model, max_new_tokens=256):
 
     # More output allowed if model has thinking mode
+    model_name = getattr(model.config, "_name_or_path", "")
     thinking_models = ["openai/gpt-oss-20b", "zai-org/GLM-4-32B-0414", "openai/gpt-oss-120b", "Qwen/Qwen3-235B-A22B", "deepseek-ai/DeepSeek-R1-0528", "deepseek-ai/DeepSeek-V3-0324"]
-    if any(model in m for m in thinking_models):
+    if any(model_name.lower() in m for m in thinking_models.lower()):
         max_new_tokens = 1024
 
     from mistral_common.protocol.instruct.request import ChatCompletionRequest
