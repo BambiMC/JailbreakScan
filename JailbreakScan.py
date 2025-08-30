8f791d6f15975e8d38a25ffd9ff57daae4cb6cab
BambiMC <fabians.viper@arcor.de>
2025-08-30 17:38:55 +0200

    debug more

--- JailbreakScan.py
+++ JailbreakScan.py
@@ -290,11 +290,11 @@ def main():
 
     print(" --- Starting rewriting prompts pass --- ")
     if args.rewrite_prompts:
-        # rewriter_model = HF_Model("google/gemma-3-12b-it")
-        # rewriter_model.load_model("google/gemma-3-12b-it", load_in_4bit=True, multi_gpu=args.multi_gpu)
+        # rewriter_model = HF_Model("dphn/Dolphin-Llama3.1-8B-Instruct-6.0bpw-h6-exl2")
+        # rewriter_model.load_model("dphn/Dolphin-Llama3.1-8B-Instruct-6.0bpw-h6-exl2", load_in_4bit=True, multi_gpu=args.multi_gpu)
         model = HF_Model(args.model_name)
         model.load_model(args.model_name, load_in_4bit=True, multi_gpu=args.multi_gpu)
-
+        print("\n\n\nREWRITER IST GELADEN\n\n\n")
         # rewritten_outputs = rewriter_model.rewrite_prompts(prompts, 512)
         # rewritten_prompts = strip_input_from_output(rewritten_outputs, prompts)
         # prompts = [p.split('"')[1] if '"' in p else p for p in rewritten_prompts]
