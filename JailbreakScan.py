2959b2fa3a2b12fc2b56c105a789fa1d9f4889e7
BambiMC <fabians.viper@arcor.de>
2025-08-30 17:40:26 +0200

    im lost

--- JailbreakScan.py
+++ JailbreakScan.py
@@ -290,10 +290,10 @@ def main():
 
     print(" --- Starting rewriting prompts pass --- ")
     if args.rewrite_prompts:
-        # rewriter_model = HF_Model("dphn/Dolphin-Llama3.1-8B-Instruct-6.0bpw-h6-exl2")
-        # rewriter_model.load_model("dphn/Dolphin-Llama3.1-8B-Instruct-6.0bpw-h6-exl2", load_in_4bit=True, multi_gpu=args.multi_gpu)
-        model = HF_Model(args.model_name)
-        model.load_model(args.model_name, load_in_4bit=True, multi_gpu=args.multi_gpu)
+        model = HF_Model("dphn/Dolphin-Llama3.1-8B-Instruct-6.0bpw-h6-exl2")
+        model.load_model("dphn/Dolphin-Llama3.1-8B-Instruct-6.0bpw-h6-exl2", load_in_4bit=True, multi_gpu=args.multi_gpu)
+        # model = HF_Model(args.model_name)
+        # model.load_model(args.model_name, load_in_4bit=True, multi_gpu=args.multi_gpu)
         print("\n\n\nREWRITER IST GELADEN\n\n\n")
         # rewritten_outputs = rewriter_model.rewrite_prompts(prompts, 512)
         # rewritten_prompts = strip_input_from_output(rewritten_outputs, prompts)
